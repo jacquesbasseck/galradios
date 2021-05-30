f6396d360c06223a59f4aa313863c425914ce2b9
Jacques BASSECK <jacquesbasseck@gmail.com>
2021-05-30 23:31:50 +0200

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { CaretUpOutlined, HeartOutlined, HeartTwoTone } from '@ant-design/icons';
+import { HeartFill, CaretUpFill } from "react-bootstrap-icons";
 
 export default function Footer() {
     return (
         <footer className="foot">
             <div className="container">
                 <p className="float-end mb-1">
-                    <a href="#"><CaretUpOutlined /></a>
+                    <a href="#"><CaretUpFill /></a>
                 </p>
                 <div className="section-1">
                     <p className="mb-1">&copy; {new Date().getFullYear()}</p>
                 </div>
                 <div className="last-section">
                     <div>
-                        Made with <span className="coeur"> <HeartTwoTone twoToneColor="#7c0a02" /></span> Jabass
+                        Made with <span className="coeur"> <HeartFill color="#7c0a02" /></span> Jabass
                     </div>
                 </div>
             </div>
